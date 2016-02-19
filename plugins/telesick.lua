ae318768a9dff1780d332e28dae11885464f4258
siktirmirza <alicowman79@gmail.com>
2016-02-19 15:52:23 +0330

    Create telesick.lua

---
+++ plugins/telesick.lua
@@ -0,0 +1,34 @@
+do
+
+function run(msg, matches)
+return [[ 
+Tele Sick Anti Spam Bot Ver . 8.5
+Ali Destroyer [Founder][Coder][Sudo]
+Matin Shadow [Tester]
+Manan.k [Tester]
+Mohammad TicTic [Helper]
+Pooria.w [Supporter]
+Jan123 [Supporter]
+MehranUX [Sucker :D]
+have any question ? @shopbuy
+]]
+end
+
+return {
+description = "Shows bot about text", 
+usage = " TeleSick : Show Creators",
+patterns = {
+"^telesick$"
+"^TeleSick$"
+"^Telesick$"
+"^!telesick$"
+"^!TeleSick$"
+"^!Telesick$"
+"^/telesick$"
+"^/TeleSick$"
+"^/Telesick$"
+}, 
+run = run 
+}
+
+end
